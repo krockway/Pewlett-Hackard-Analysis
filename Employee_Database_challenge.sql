5126a2bb981eef894725959a367afb0f8d30db42
Kaitlin Rockway <krockway@gmail.com>
2021-03-14 22:10:36 -0700

    Deliverable 2 complete

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -36,3 +36,27 @@ FROM retire_title_unique
 GROUP BY title
 ORDER BY count DESC
 
+-- Deliverable 2: The Employees Eligible for the Mentorship Program
+-- Retrieve the emp_no, first_name, last_name, and birth_date columns from the Employees table.
+-- Retrieve the from_date and to_date columns from the Department Employee table.
+-- Retrieve the title column from the Titles table.
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+-- Create a new table using the INTO clause.
+INTO metorship_eligibility
+FROM employees as e
+-- Join the Employees and the Department Employee tables on the primary key.
+JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+-- Join the Employees and the Titles tables on the primary key.
+JOIN titles as t
+ON (e.emp_no = t.emp_no)
+-- Filter the data on the to_date column to get current employees whose birth dates are between January 1, 1965 and December 31, 1965.
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+-- Order the table by the employee number.
+ORDER BY e.emp_no ASC
